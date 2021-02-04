2814f38d08f80fc15d485252482d762c94ac667a
Camila-Santana <tst.santana.camila@gmail.com>
2021-02-04 11:21:18 -0300

    Create acessando_item_array.js

---
+++ acessando_item_array.js
@@ -0,0 +1,22 @@
+// ARRAY //
+
+// ACESSANDO ITEM //
+
+// você acessa fazendo referencia a que posição ele ocupa
+// atribuindo o [] colchete e  o número a que posição ele se refere
+// o número começa com 0 zero
+
+// Vamos à prática //
+
+var frutas = ['maça', 'banana', 'caqui']
+console.log(frutas[0])
+
+//EXPLICAÇÃO//
+
+//criei um uma VARIÁVEL, cujo IDENTIFICADOR/NOME é frutas
+// então, esse identificador RECEBE os ARRAYS [maça, banana, caqui]
+// feito isso, eu ACESSO um dos ARRAYS criados, escolhendo um número
+// COMO FAÇO ISSO?
+// lembra da VARIÁVEL declarada?
+// uso o comando CONSOLE.LOG mais o nome da VARIÁVEL e um número aleatório
+// pronto, o pc vai me mostrar que o número que escolhi, reprenta a posição do meu ARRAY CRIADO
