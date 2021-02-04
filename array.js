07ffef0689858f82229c62c5eb45c1bb90cb64e4
Camila-Santana <tst.santana.camila@gmail.com>
2021-02-04 10:45:58 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+// Array OU Vetores //
+// o Array refere-se a posições, agrupamentos dos objetos/elementos//
+
+// COMO ESCREVER OS ARRAYS?//
+// usando [] colchetes//
+
+
+// Vamos à prática//
+
+// escrevendo os arrays e mostrando na tela as informações dos mesmos //
+
+var nomeDeAlunos = ['Mayk', 'Suliman', 'Saymon']
+console.log(nomeDeAlunos)
